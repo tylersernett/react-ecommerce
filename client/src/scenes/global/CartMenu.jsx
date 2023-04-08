4e377c1145804d37f595f2b7aff8f7d002c6bb2f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-08 15:28:43 -0400

    Add drawer: can open

--- client/src/scenes/global/CartMenu.jsx
+++ client/src/scenes/global/CartMenu.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, IconButton, Typography } from "@mui/material";
+import { Box, Button, Divider, IconButton, Typography, Drawer } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import CloseIcon from "@mui/icons-material/Close";
 import AddIcon from "@mui/icons-material/Add";
@@ -22,123 +22,168 @@ const CartMenu = () => {
     const isCartOpen = useSelector((state) => state.cart.isCartOpen);
 
     const totalPrice = cart.reduce((total, item) => {
-        return total + item.count * item.attributes.price;
+        return total + (item.count * item.attributes.price);
     }, 0);
 
 
     return (
-        <Box //OVERLAY
-            display={isCartOpen ? "block" : "none"}
-            backgroundColor="rgba(0, 0, 0, 0.65)"
-            position="fixed"
-            zIndex={10}
-            width="100%"
-            height="100%"
-            left="0"
-            top="0"
-            overflow="auto"
-        >
-            <Box //MODAL
+        <Drawer anchor="right" open={isCartOpen} >
+            <Box //OVERLAY
+                // display={isCartOpen ? "block" : "none"}
+                backgroundColor="rgba(0, 0, 0, 0.65)"
                 position="fixed"
-                right="0"
-                bottom="0"
-                width="max(400px, 30%)"
+                // zIndex={10}
+                width="100%"
                 height="100%"
-                backgroundColor="rgba(0, 0, 0, 0.8)"
+                left="0"
+                top="0"
+                overflow="auto"
             >
-                <Box padding="30px" overflow="auto" height="100%">
-                    {/* HEADER */}
-                    <FlexBox mb="15px">
-                        <Typography variant="h3" >SHOPPING BAG ({cart.length})</Typography>
-                        <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
-                            <CloseIcon />
-                        </IconButton>
-                    </FlexBox>
-
-                    {/* CART LIST */}
-                    <Box>
-                        {cart.map((item) => (
-                            <Box key={`${item.attributes.name}-${item.id}`}>
-                                <FlexBox p="15px 0">
-                                    <Box flex="1 1 40%">
-                                        {/* shrink:1, grow: 1 */}
-                                        <img
-                                            alt={item?.name}
-                                            width="123px"
-                                            height="123px"
-                                            src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
-                                        />
-                                    </Box>
-                                    <Box flex="1 1 60%">
-                                        <FlexBox mb="5px">
-                                            <Typography fontWeight="bold">
-                                                {item.attributes.name}
-                                            </Typography>
-                                            <IconButton onClick={() => dispatch(removeFromCart({ id: item.id }))}>
-                                                <CloseIcon />
-                                            </IconButton>
-                                        </FlexBox>
-                                        <Typography>{item.attributes.shortDescription}</Typography>
-                                        <FlexBox m="15px 0">
-                                            <Box
-                                                display="flex"
-                                                alignItems="center"
-                                                border={`1.5px solid ${shades.neutral[500]}`}
-                                            >
-                                                <IconButton onClick={() => dispatch(decreaseCount({ id: item.id }))}>
-                                                    <RemoveIcon />
-                                                </IconButton>
-                                                <Typography>{item.count}</Typography>
-                                                <IconButton onClick={() => dispatch(increaseCount({ id: item.id }))}>
-                                                    <AddIcon />
-                                                </IconButton>
-                                            </Box>
-
-                                            {/* PRICE */}
-                                            <Typography fontWeight="">
-                                                ${item.attributes.price}
-                                            </Typography>
-                                        </FlexBox>
-                                    </Box>
-                                </FlexBox>
-                                <Divider />
-                            </Box>
-                        ))}
-
-                    </Box>
-
-                    {/* ACTIONS */}
-                    <Box m="20px 0">
-                        <FlexBox m="20px 0">
-                            <Typography fontWeight="bold" >SUBTOTAL</Typography>
-                            <Typography fontWeight="bold">${totalPrice}</Typography>
+                <Box //MODAL
+                    position="fixed"
+                    right="0"
+                    bottom="0"
+                    width="max(400px, 30%)"
+                    height="100%"
+                    backgroundColor="rgba(0, 0, 0, 0.8)"
+                >
+                    <Box padding="30px" overflow="auto" height="100%">
+                        {/* HEADER */}
+                        <FlexBox mb="15px">
+                            <Typography variant="h3" >SHOPPING BAG ({cart.length})</Typography>
+                            <IconButton onClick={() => dispatch(setIsCartOpen({}))}>
+                                <CloseIcon />
+                            </IconButton>
                         </FlexBox>
-                        <Button
-                            sx={{
-                                backgroundColor: shades.secondary[600],
-                                color: 'white',
-                                borderRadius: 0,
-                                minWidth: "100%",
-                                padding: "20px 40px",
-                                fontWeight: "bold",
-                                m: "20px 0",
-                                "&:hover": {
-                                    color: 'black',
-                                    backgroundColor: shades.secondary[500],
-                                }
-                            }}
-                            onClick={() => {
-                                navigate("/checkout");
-                                dispatch(setIsCartOpen({}));
-                            }}
-                        >
-                            CHECKOUT
-                        </Button>
+
+                        {/* CART LIST */}
+                        <Box>
+                            {cart.map((item) => (
+                                <Box key={`${item.attributes.name}-${item.id}`}>
+                                    <FlexBox p="15px 0">
+                                        <Box flex="1 1 40%">
+                                            {/* shrink:1, grow: 1 */}
+                                            <img
+                                                alt={item?.name}
+                                                width="123px"
+                                                height="123px"
+                                                src={`http://localhost:1337${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+                                            />
+                                        </Box>
+                                        <Box flex="1 1 60%">
+                                            <FlexBox mb="5px">
+                                                <Typography fontWeight="bold">
+                                                    {item.attributes.name}
+                                                </Typography>
+                                                <IconButton onClick={() => dispatch(removeFromCart({ id: item.id }))}>
+                                                    <CloseIcon />
+                                                </IconButton>
+                                            </FlexBox>
+                                            <Typography>{item.attributes.shortDescription}</Typography>
+                                            <FlexBox m="15px 0">
+                                                <Box
+                                                    display="flex"
+                                                    alignItems="center"
+                                                    border={`1.5px solid ${shades.neutral[500]}`}
+                                                >
+                                                    <IconButton onClick={() => dispatch(decreaseCount({ id: item.id }))}>
+                                                        <RemoveIcon />
+                                                    </IconButton>
+                                                    <Typography>{item.count}</Typography>
+                                                    <IconButton onClick={() => dispatch(increaseCount({ id: item.id }))}>
+                                                        <AddIcon />
+                                                    </IconButton>
+                                                </Box>
+
+                                                {/* PRICE */}
+                                                <Typography fontWeight="">
+                                                    ${item.attributes.price}
+                                                </Typography>
+                                            </FlexBox>
+                                        </Box>
+                                    </FlexBox>
+                                    <Divider />
+                                </Box>
+                            ))}
+
+                        </Box>
+
+                        {/* ACTIONS */}
+                        <Box m="20px 0">
+                            <FlexBox m="20px 0">
+                                <Typography fontWeight="bold" >SUBTOTAL</Typography>
+                                <Typography fontWeight="bold">${totalPrice}</Typography>
+                            </FlexBox>
+                            <Button
+                                sx={{
+                                    backgroundColor: shades.secondary[600],
+                                    color: 'white',
+                                    borderRadius: 0,
+                                    minWidth: "100%",
+                                    padding: "20px 40px",
+                                    fontWeight: "bold",
+                                    m: "20px 0",
+                                    "&:hover": {
+                                        color: 'black',
+                                        backgroundColor: shades.secondary[500],
+                                    }
+                                }}
+                                onClick={() => {
+                                    navigate("/checkout");
+                                    dispatch(setIsCartOpen({}));
+                                }}
+                            >
+                                CHECKOUT
+                            </Button>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
-        </Box>
+        </Drawer>
     )
 }
 
 export default CartMenu
+
+/*
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+
+export default function TemporaryDrawer() {
+    const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+    const toggleDrawer = (bool) => (event) => {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            setDrawerOpen(false);
+            return;
+        }
+
+        setDrawerOpen(bool);
+    };
+
+    const drawerContent = () => (
+        <Box
+            sx={{ width: 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            DRAWER CONTENTS HERE
+        </Box>
+    );
+
+    return (
+        <>
+            <Button onClick={toggleDrawer(true)}>{"right"}</Button>
+            <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+                {drawerContent()}
+            </Drawer>
+        </>
+    );
+}
+
+instead of changing display:none, change drawer {open} prop
+*/
