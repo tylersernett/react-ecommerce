41753fab21153403a8725a9a0f2429469eef97ab
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-14 02:16:19 -0400

    Add Threshold360 tour

--- client/src/scenes/appointments/Appointments.jsx
+++ client/src/scenes/appointments/Appointments.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+
+const key = process.env.REACT_APP_THRESHOLD_KEY;
 
 const Appointments = () => {
   return (
-    <Box m="100px auto" width="80%" height="50vh">
-      <div>Appointments</div>
+    <Box m="100px auto" width="80%" mx='auto' >
+
+      <Box textAlign='center'>
+      <Typography fontSize={'1rem'} my='20px'>Can't come in person? Take a virtual tour below.</Typography>
+      <iframe title='threshold360'
+        src={`https://cloud.threshold360.com/embed/locations/8447298?webKey=${key}`}
+        width="600"
+        height="400"
+        style={{ border: '0px' }}
+        allowFullScreen={true}
+      >
+      </iframe>
+      </Box>
+      
     </Box>
+
   )
 }
 
