7188fbdebe25512a54daabb74b248fc2db912d31
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-20 16:23:21 -0400

    Tweak vh amount

--- client/src/scenes/gallery/Gallery.jsx
+++ client/src/scenes/gallery/Gallery.jsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 
 const Gallery = () => {
   return (
-    <Box m="100px auto" width="80%" height="50vh">
+    <Box m="100px auto" width="80%" height="42vh">
       <div>Gallery</div>
     </Box>
   )
