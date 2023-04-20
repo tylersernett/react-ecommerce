86c612fb3b360ffa6b6b19b1ea57cd7d8b4e61f8
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-20 14:36:15 -0400

    Add thankyou page

---
+++ client/src/scenes/events/Thankyou.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box, Typography } from "@mui/material";
+
+const Thankyou = () => {
+  return (
+    <Box m="100px auto" width="80%" height="30vh">
+      <Typography>Thank you—your message has been received. We'll be in touch soon.</Typography>
+    </Box>
+  )
+}
+
+export default Thankyou
