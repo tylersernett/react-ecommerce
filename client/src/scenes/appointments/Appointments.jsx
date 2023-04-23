3f150c212641c3d6c9861d5820eeb2f35073db4a
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-23 17:22:44 -0400

    Add AppointmentBooking component

--- client/src/scenes/appointments/Appointments.jsx
+++ client/src/scenes/appointments/Appointments.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Box, Typography, useMediaQuery } from "@mui/material";
 import { shades } from "../../theme";
+import AppointmentBooking from './AppointmentBooking';
 const key = process.env.REACT_APP_THRESHOLD_KEY;
 
 const Appointments = () => {
@@ -11,13 +12,14 @@ const Appointments = () => {
         <Typography variant='h2' textAlign='center' my='20px' color={shades.secondary[400]}>
           Private Tours
         </Typography>
-        <Typography fontSize={'1rem'}>
+        <Typography fontSize={'1rem'} mb='20px'>
           Whether it's in preparation for an upcoming
           birthday, holiday, or you'd just like to visit,
           if you're interested in taking a private tour of The Hood,
           you can find our guidelines and appointment info below.
           {/* We'll see you soon! */}
         </Typography>
+        <AppointmentBooking />
         <Box textAlign='center'>
           <Typography fontSize={'1.5rem'} my='20px'>Can't come in person? Take a <span style={{ color: shades.secondary[400] }}>virtual tour</span> below.</Typography>
           <iframe title='threshold360'
