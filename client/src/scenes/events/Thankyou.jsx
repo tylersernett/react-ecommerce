5b2e9c35190e89a0fb660f0fdf9ce238a32bf4bd
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-27 01:44:02 -0400

    Add coming soon message

--- client/src/scenes/events/Thankyou.jsx
+++ client/src/scenes/events/Thankyou.jsx
@@ -4,7 +4,7 @@ import { Box, Typography } from "@mui/material";
 const Thankyou = () => {
   return (
     <Box m="100px auto" width="80%" height="42vh">
-      <Typography>Thank you—your message has been received. We'll be in touch soon.</Typography>
+      <Typography>Coming soon!</Typography>
     </Box>
   )
 }
