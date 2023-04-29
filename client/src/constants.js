06582d339e901e2e9c5b33fbbfdf5ad8459185b8
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-04-29 15:00:56 -0400

    Update img url and prod API url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 const prod = {  
     url: {
         BASE_URL: 'https://tylersernett.github.io/react-ecommerce',
-        API_URL: 'https://the-hood-strapi-backend.up.railway.app',
+        API_URL: 'https://the-hood-backend-strapi-fly.fly.dev',
         IMG_URL: '',
         //API_URL_USERS: 'https://myapp.herokuapp.com/users'
     }
@@ -11,7 +11,7 @@ const dev = {
     url: {
         BASE_URL: 'http://localhost:2000/react-ecommerce',
         API_URL: 'http://localhost:1337',
-        IMG_URL: 'http://localhost:1337',
+        IMG_URL: '',
     }
 };
 
